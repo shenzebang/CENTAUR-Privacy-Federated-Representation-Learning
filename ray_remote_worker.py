a4f9724cd1ea574280206f48ed32cc860427755c
shenzebang <shenzebang@gmail.com>
2022-08-05 13:57:48 +0800

    fix a bug for personalized method

--- ray_remote_worker.py
+++ ray_remote_worker.py
@@ -13,27 +13,43 @@ class Worker:
 
 def compute_with_remote_workers(remote_workers, clients, PEs):
     if remote_workers is not None:
+        # Use remote workers to accelerate computation.
         num_remote_workers = len(remote_workers)
+        num_clients = len(clients)
+        n_clients_per_worker = [int(num_clients/num_remote_workers)] * num_remote_workers
+        for i in range(num_clients % num_remote_workers):
+            n_clients_per_worker[i] += 1
+
+        print(n_clients_per_worker)
         jobs_clients = {
-            wid: [] for wid in range(num_remote_workers)
+            wid: None for wid in range(num_remote_workers)
         }
         jobs_PEs = {
-            wid: [] for wid in range(num_remote_workers)
+            wid: None for wid in range(num_remote_workers)
         }
-        wid = 0 # worker id
-        for client, PE in zip(clients, PEs):
-            jobs_clients[wid].append(client)
-            jobs_PEs[wid].append(PE)
+        cid = 0 # client id
+        for wid in range(num_remote_workers):
+            jobs_clients[wid] = clients[cid: cid + n_clients_per_worker[wid]]
+            jobs_PEs[wid] = PEs[cid: cid + n_clients_per_worker[wid]]
+            cid += n_clients_per_worker[wid]
 
-            wid = (wid + 1) % num_remote_workers
 
-        job_ids = [remote_worker.step.remote(jobs_clients[idx], jobs_PEs[idx])
-                  for idx, remote_worker in enumerate(remote_workers)]
+            # Uncomment for the purpose of DEBUG
+            # client_ids = [client.idx for client in jobs_clients[wid]]
+            # print(
+            #     f"Worker {wid} handles clients {client_ids}."
+            # )
 
-        results = ray.get(job_ids)
 
+        ray_job_ids = [remote_worker.step.remote(jobs_clients[wid], jobs_PEs[wid])
+                  for wid, remote_worker in enumerate(remote_workers)]
+        # Calling remote functions only creates job ids. Use ray.get() to actually carry out these jobs.
+        results = ray.get(ray_job_ids)
+
+        # IMPORTANT: "results" should have the same order as "clients" and "PEs"! Incorrect ordering will cause BUG!
         results = list(itertools.chain.from_iterable(results))
     else:
+        # No remote workers are available. Simply evaluate sequentially.
         results = [client.step(PE) for client, PE in zip(clients, PEs)]
 
     return results
--- script/run_DP-FedRep.sh
+++ script/run_DP-FedRep.sh
@@ -1,34 +1,35 @@
 args=(DP-FedRep.py
-    #  model configuration
-     --model cnn
-    #  dataset configuration
-     --dataset cifar10
-     --shard_per_user 2
-     --num_classes 10
-    #  experiment configuration
-#      --data_augmentation
-     --epochs 100
-     --seed 1
-     --num_users 100
-    #  DP configuration
-     --disable-dp\
-     --epsilon 1
-     --delta 1e-5
-     --dp_clip 1
-    #  save/load configuration
-    #  backend configuration
-     --gpu 0-1-2-3
-     --ray_gpu_fraction 0.3
-     --use_ray
-    #  test configuration
-    #  train configuration
-     --lr-rep 1e-1
-     --lr-head 1e-2
-     --local_head_ep 15
-     --local_ep 1
-     --batch_size 4000
-     --MAX_PHYSICAL_BATCH_SIZE 100
-#      --verbose
+        #  model configuration
+        --model cnn
+        #  dataset configuration
+        --dataset cifar10
+        --shard_per_user 2
+        --num_classes 10
+        #  experiment configuration
+        #      --data_augmentation
+        --epochs 100
+        --seed 1
+        --num_users 100
+        #  DP configuration
+        #      --disable-dp
+        --epsilon 1
+        --delta 1e-5
+        --dp_clip 1
+        #  save/load configuration
+        #  backend configuration
+        --gpu 0-1-2-3
+        --use_ray
+        --ray_gpu_fraction 0.3
+        #  test configuration
+        #  train configuration
+        --batch_size 4000
+        --MAX_PHYSICAL_BATCH_SIZE 100
+        --local_ep 1
+        # --verbose
+        # algorithm specific configuration
+        --lr-rep 1e-1
+        --lr-head 1e-2
+        --local_head_ep 15
      )
 
 python "${args[@]}"
