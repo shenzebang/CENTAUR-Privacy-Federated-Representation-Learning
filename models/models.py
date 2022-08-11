994c85300a88babd422be38c7ed18c7d5f39931f
shenzebang <shenzebang@gmail.com>
2022-08-11 13:08:10 +0800

    add the neural net models for CIFAR100

--- models/models.py
+++ models/models.py
@@ -71,9 +71,77 @@ class CNNCifar10_BN(nn.Module):  # with batch normalization
         x = self.fc3(x)
         return x
 
+class CNNCifar100(nn.Module):
+    def __init__(self, args):
+        super(CNNCifar100, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        # self.drop = nn.Dropout(0.6)
+        self.conv2 = nn.Conv2d(64, 128, 5)
+        self.fc1 = nn.Linear(128 * 5 * 5, 384)
+        self.fc2 = nn.Linear(384, 192)
+        self.fc3 = nn.Linear(192, args.num_classes)
+        self.cls = args.num_classes
+
+        self.weight_keys = [['fc1.weight', 'fc1.bias'],
+                            ['fc2.weight', 'fc2.bias'],
+                            ['fc3.weight', 'fc3.bias'],
+                            ['conv2.weight', 'conv2.bias'],
+                            ['conv1.weight', 'conv1.bias'],
+                            ]
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 128 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class CNNCifar100_BN(nn.Module):  # with batch normalization
+    def __init__(self, args):
+      super(CNNCifar100_BN, self).__init__()
+      self.conv1 = nn.Conv2d(3, 64, 5)
+      self.conv1_bn=nn.BatchNorm2d(64)
+      self.pool = nn.MaxPool2d(2, 2)
+      self.drop = nn.Dropout(0.6)
+      self.conv2 = nn.Conv2d(64, 128, 5)
+      self.conv2_bn=nn.BatchNorm2d(128)
+      self.fc1 = nn.Linear(128 * 5 * 5, 256)
+      self.fc1_bn=nn.BatchNorm1d(256)
+      self.fc2 = nn.Linear(256, 128)
+      self.fc2_bn=nn.BatchNorm1d(128)
+      self.fc3 = nn.Linear(128, args.num_classes)
+      self.cls = args.num_classes
+
+      self.weight_keys = [['fc1.weight', 'fc1.bias'],
+                          ['fc2.weight', 'fc2.bias'],
+                          ['fc3.weight', 'fc3.bias'],
+                          ['conv2.weight', 'conv2.bias'],
+                          ['conv1.weight', 'conv1.bias'],
+                          ['fc1_bn.weight', 'fc1_bn.bias', 'fc1_bn.running_mean', 'fc1_bn.running_var', 'fc1_bn.num_batches_tracked'],
+                          ['fc2_bn.weight', 'fc2_bn.bias', 'fc2_bn.running_mean', 'fc2_bn.running_var', 'fc2_bn.num_batches_tracked'],
+                          ['conv1_bn.weight', 'conv1_bn.bias', 'conv1_bn.running_mean', 'conv1_bn.running_var', 'conv1_bn.num_batches_tracked'],
+                          ['conv2_bn.weight', 'conv2_bn.bias', 'conv2_bn.running_mean', 'conv2_bn.running_var', 'conv2_bn.num_batches_tracked']
+                          ]
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1_bn(self.conv1(x))))
+        x = self.pool(F.relu(self.conv2_bn(self.conv2(x))))
+        x = x.view(-1, 128 * 5 * 5)
+        x = F.relu(self.fc1_bn(self.fc1(x)))
+        x = self.drop((F.relu(self.fc2_bn(self.fc2(x)))))
+        x = self.fc3(x)
+        return x
+
 def get_model(args):
     if args.model == 'cnn' and 'cifar100' in args.dataset:
-        raise NotImplementedError
+        if args.norm == 'None':
+            net_glob = CNNCifar100(args=args)
+        elif args.norm == 'batch_norm':
+            net_glob = CNNCifar100_BN(args=args)
+            net_glob = ModuleValidator.fix(net_glob)
     elif args.model == 'cnn' and 'cifar10' in args.dataset:
         if args.norm == 'None':
             net_glob = CNNCifar10(args=args)
