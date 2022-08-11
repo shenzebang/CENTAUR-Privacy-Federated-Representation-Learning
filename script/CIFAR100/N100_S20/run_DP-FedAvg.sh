81f3d6bbe3156ffb14087949b46aa9d9cf68d7f0
shenzebang <shenzebang@gmail.com>
2022-08-11 13:00:26 +0800

    Add the scripts for cifar100

---
+++ script/CIFAR100/N100_S20/run_DP-FedAvg.sh
@@ -0,0 +1,37 @@
+args=(main.py
+        #  algorithm configuration
+        --alg DP_FedAvg_ft
+        #  model configuration
+        --model cnn
+        #  dataset configuration
+        --dataset cifar100
+        --shard_per_user 20
+        --num_classes 100
+        #  experiment configuration
+        #      --data_augmentation
+        --epochs 300
+        --seed 1
+        --num_users 100
+        #  DP configuration
+        #      --disable-dp
+        --epsilon 1
+        --delta 1e-5
+        --dp_clip 1
+        #  save/load configuration
+        #  backend configuration
+        --gpu 0-1-2-3
+        --use_ray
+        --ray_gpu_fraction 0.3
+        #  test configuration
+        #  train configuration
+        --batch_size 4000
+        --MAX_PHYSICAL_BATCH_SIZE 256
+        --local_ep 1
+        # --verbose
+        # algorithm specific configuration
+        --lr 1e-1
+        --lr-head 1e-2
+        --ft-ep 15
+     )
+
+python "${args[@]}"
