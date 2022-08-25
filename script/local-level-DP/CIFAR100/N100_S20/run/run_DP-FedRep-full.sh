6006609d23bc99e6b07b7362c4f9cfab2c073fbe
penglingwei <penglingwei@zju.edu.cn>
2022-08-25 16:29:00 +0800

    update the configuration script/local-level-DP/CIFAR100/N100_S20/run/run_DP-FedRep-full.sh

--- script/local-level-DP/CIFAR100/N100_S20/run/run_DP-FedRep-full.sh
+++ script/local-level-DP/CIFAR100/N100_S20/run/run_DP-FedRep-full.sh
@@ -17,19 +17,20 @@ args=(--alg DP_FedRep
     --dp_clip 1
     #  save/load configuration
     #  backend configuration
-#     --use_ray
-#     --ray_gpu_fraction .5
+    --use_ray
+    --ray_gpu_fraction .5
     #  test configuration
+    --print_freq 2
     #  train configuration
     --frac_participate 1
-    --batch_size 4000
+    --batch_size 500
     --MAX_PHYSICAL_BATCH_SIZE 64
-    --local_ep 1
+    --local_ep 4
     # --verbose
     # algorithm specific configuration
-    --lr 1e-1
+    --lr .01 
     --lr-head 1e-2
     --local_head_ep 15
     )
 
-CUDA_VISIBLE_DEVICES=3 python main.py "${args[@]}"
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
