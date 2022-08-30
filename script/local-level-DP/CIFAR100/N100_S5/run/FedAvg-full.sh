53a2c853e0d5bc44756bfe6da65f744771103f36
shenzebang <shenzebang@gmail.com>
2022-08-30 21:24:44 +0800

    Add the script of FedAvg-full and FedRep-full for CIFAR100/N100/S5

---
+++ script/local-level-DP/CIFAR100/N100_S5/run/FedAvg-full.sh
@@ -0,0 +1,32 @@
+args=(--alg DP_FedAvg_ft
+    #  model configuration
+    --model cnn
+    #  dataset configuration
+    --dataset cifar100
+    --shard_per_user 5
+    --num_classes 100
+    #  experiment configuration
+    #         --data_augmentation
+    #         --data_augmentation_multiplicity 16
+    --epochs 400
+    --seed 1
+    --num_users 100
+    #  DP configuration
+    --disable-dp
+    #  save/load configuration
+    #  backend configuration
+    --use_ray
+    --ray_gpu_fraction .33
+    #  test configuration
+    #  train configuration
+    --frac_participate 1
+    --batch_size 100
+    --local_ep 5
+    # --verbose
+    # algorithm specific configuration
+    --lr 1e-2
+    --lr-head 1e-2
+    --ft-ep 15
+    )
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
