b3de8597b6cc38216a29730ae662863eb891f3d9
shenzebang <shenzebang@gmail.com>
2022-08-30 21:28:07 +0800

    Update DP-FedRep-full for CIFAR100/N100/S20

--- script/local-level-DP/CIFAR100/N100_S20/run/DP-FedRep-full.sh
+++ script/local-level-DP/CIFAR100/N100_S20/run/DP-FedRep-full.sh
@@ -45,28 +45,29 @@ args=(--alg DP_FedRep
     --num_classes 100
     #  experiment configuration
     #      --data_augmentation
-    --epochs 200
+    --epochs 400
     --seed 1
     --num_users 100
     #  DP configuration
     #      --disable-dp
     --epsilon 1
     --delta 1e-5
-    --dp_clip .5
+    --dp_clip .1
     #  save/load configuration
     #  backend configuration
     --use_ray
-    --ray_gpu_fraction .5
+    --ray_gpu_fraction .33
     #  test configuration
     --print_freq 5
     #  train configuration
+#     --aggr trimmed_mean
     --frac_participate 1
-    --batch_size 50
+    --batch_size 500
     --MAX_PHYSICAL_BATCH_SIZE 25
-    --local_ep 1
+    --local_ep 4
     # --verbose
     # algorithm specific configuration
-    --lr .01
+    --lr .1
     --lr-head 1e-2
     --local_head_ep 15
     )
