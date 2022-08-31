c39b9db223fd8448f6f4c400146fce9308ffede8
shenzebang <shenzebang@gmail.com>
2022-09-01 00:45:13 +0800

    Update the scripts

--- script/local-level-DP/CIFAR100/N100_S20/run/DP-FedAvg-full.sh
+++ script/local-level-DP/CIFAR100/N100_S20/run/DP-FedAvg-full.sh
@@ -7,28 +7,27 @@ args=(--alg DP_FedAvg_ft
     --num_classes 100
     #  experiment configuration
     #      --data_augmentation
-    --epochs 200
+    --epochs 400
     --seed 1
     --num_users 100
     #  DP configuration
     #      --disable-dp
     --epsilon 1
     --delta 1e-5
-    --dp_clip .5
+    --dp_clip .1
     #  save/load configuration
     #  backend configuration
-#     --gpu 0-1-2-3
     --use_ray
     --ray_gpu_fraction .33
     #  test configuration
     #  train configuration
     --frac_participate 1
-    --batch_size 50
+    --batch_size 500
     --MAX_PHYSICAL_BATCH_SIZE 25
-    --local_ep 1
+    --local_ep 4
     # --verbose
     # algorithm specific configuration
-    --lr 1e-2
+    --lr .1
     --lr-head 1e-2
     --ft-ep 15
     )
