9a300b9b0899e4a4d1d81c1cc217a14428f1c472
shenzebang <shenzebang@gmail.com>
2022-09-25 23:27:24 +0800

    Add the scripts for the utility privacy tradeoff

---
+++ script/user-level-DP/utility-privacy-tradeoff/DP-FedRep-full-16.sh
@@ -0,0 +1,38 @@
+args=(--alg DP_FedRep
+    #  model configuration
+    --model cnn
+    #  dataset configuration
+    --dataset cifar100
+    --shard_per_user 20
+    --num_classes 100
+    #  experiment configuration
+#     --data_augmentation
+#     --data_augmentation_multiplicity 16
+    --epochs 200
+    --seed 1
+    --num_users 500
+    --n_runs 1
+    #  DP configuration
+    #      --disable-dp
+#     --noise_multiplier 1
+    --dp_type user-level-DP
+    --epsilon 16
+    --delta 1e-5
+    --dp_clip .16
+    #  save/load configuration
+    #  backend configuration
+    --use_ray
+    --ray_gpu_fraction .33
+    #  test configuration
+    #  train configuration
+    --frac_participate 1
+    --batch_size 100
+    --local_ep 1
+    # --verbose
+    # algorithm specific configuration
+    --lr 1e-2
+    --lr-head 1e-2
+    --local_head_ep 15
+    )
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
---
+++ script/user-level-DP/utility-privacy-tradeoff/PPSGD-full-4.sh
@@ -0,0 +1,37 @@
+args=(#  algorithm configuration
+        --alg PPSGD
+        #  model configuration
+        --model cnn
+        #  dataset configuration
+        --dataset cifar100
+        --shard_per_user 20
+        --num_classes 100
+        #  experiment configuration
+        #      --data_augmentation
+        --epochs 200
+        --seed 1
+        --num_users 500
+        --n_runs 1
+        #  DP configuration
+        #      --disable-dp
+        --dp_type user-level-DP
+        --epsilon 4
+        --delta 1e-5
+        --dp_clip .04
+        #  save/load configuration
+        #  backend configuration
+        --use_ray
+        --ray_gpu_fraction 0.33
+        #  test configuration
+        #  train configuration
+        --frac_participate 1
+        --batch_size 100
+        --local_ep 1
+        # --verbose
+        # algorithm specific configuration
+        --lr 1e-2
+        --lr-head 1e-1
+        --local_head_ep 1
+     )
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
