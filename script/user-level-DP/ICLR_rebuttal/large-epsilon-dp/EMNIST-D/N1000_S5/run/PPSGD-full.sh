179a52cc82e64724a11f04ef9d5e0b0001187ddb
zebang <shenzebang@gmail.com>
2022-12-09 13:48:05 +0100

    Update the scripts for ICLR rebuttal

---
+++ script/user-level-DP/ICLR_rebuttal/large-epsilon-dp/EMNIST-D/N1000_S5/run/PPSGD-full.sh
@@ -0,0 +1,39 @@
+args=(--alg PPSGD
+    #  model configuration
+    --model mlp
+    #  dataset configuration
+    --dataset emnist_d
+    --shard_per_user 5
+    --num_classes 10
+    #  experiment configuration
+    #         --data_augmentation
+    #         --data_augmentation_multiplicity 16
+    --epochs 40
+    --seed 1
+    --num_users 1000
+    --n_runs 3
+    #  DP configuration
+    #      --disable-dp
+    --dp_type user-level-DP
+    --epsilon 50
+    --delta 1e-5
+    --dp_clip .5
+    #  save/load configuration
+    #  backend configuration
+    --use_ray
+    --ray_gpu_fraction .25
+    #  test configuration
+    --print_freq 2
+    --print_diff_norm
+    #  train configuration
+    --frac_participate 1
+    --batch_size 100
+    --local_ep 2
+    # --verbose
+    # algorithm specific configuration
+    --lr 1e-2
+    --lr-head 1e-1
+    --local_head_ep 1
+    )
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
