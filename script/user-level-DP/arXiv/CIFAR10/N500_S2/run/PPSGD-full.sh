be79a0c2831c01b8c9f04e420777efddc5cc7646
shenzebang <shenzebang@gmail.com>
2022-12-19 20:53:40 +0100

    add scripts for arxiv-cifar10

---
+++ script/user-level-DP/arXiv/CIFAR10/N500_S2/run/PPSGD-full.sh
@@ -0,0 +1,39 @@
+args=(#  algorithm configuration
+        --alg PPSGD
+        #  model configuration
+        --model cnn
+        #  dataset configuration
+        --dataset cifar10
+        --shard_per_user 2
+        --num_classes 10
+        #  experiment configuration
+        #      --data_augmentation
+        --epochs 200
+        --seed 1
+        --num_users 500
+        --n_runs 3
+        #  DP configuration
+        #      --disable-dp
+        --dp_type user-level-DP
+        --epsilon 1
+        --delta 1e-5
+        --dp_clip .01
+        #  save/load configuration
+        #  backend configuration
+        --use_ray
+        --ray_gpu_fraction 0.33
+        #  test configuration
+        --print_freq 10
+        --print_diff_norm
+        #  train configuration
+        --frac_participate 1
+        --batch_size 100
+        --local_ep 1
+        # --verbose
+        # algorithm specific configuration
+        --lr 1e-2
+        --lr-head 1e-1
+        --local_head_ep 1
+     )
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
