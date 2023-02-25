e1b85a28722a9461d333e2cac440e38e17df2e8e
zebang <shenzebang@gmail.com>
2023-02-25 21:40:28 +0100

    Update the scripts for ICLR2023 Camera-ready version. On EMNIST

---
+++ script/user-level-DP/camera-ready-ICLR2023/small-epoch-data-augmentation-fine-tune/EMNIST-D/N2000_S5/run/Local.sh
@@ -0,0 +1,30 @@
+args=(--alg Local
+    #  dataset configuration
+    --dataset emnist_d
+    --num_classes 10
+    #  model configuration
+    --model mlp
+    #  experiment configuration
+#     --data_augmentation
+    --epochs 1
+    --num_users 2000
+    --shard_per_user 5
+    --seed 1
+    --n_runs 3
+    #  DP configuration
+    --disable-dp
+    #  save/load configuration
+    #  backend configuration
+#     --gpu 0-1-2-3
+    --use_ray
+    --ray_gpu_fraction .25
+    #  test configuration
+    #  train configuration
+#     --verbose
+    --lr 1e-2
+    --batch_size 100
+    --local_ep 200
+    --momentum 0
+    )
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py "${args[@]}"
